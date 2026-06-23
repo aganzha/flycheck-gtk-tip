0c877aeff85954b7e1b4543cbd020d2d2a418948
Alexey Ganzha <aganzha@yandex.ru>
2026-06-23 18:52:46 +0200

    render pango in emacs

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -3,6 +3,9 @@ use emacs::{defun, Env, Result, Value};
 use gtk::glib;
 use gtk::prelude::*;
 use std::sync::{Once, OnceLock, RwLock};
+use cairo::{Context, Format, ImageSurface};
+use pango::FontDescription;
+use pangocairo;
 
 emacs::plugin_is_GPL_compatible!();
 
@@ -20,6 +23,24 @@ pub enum Event {
     Best(String),
 }
 
+fn render_text_offscreen(text: &str, font: &str, size: f64) -> ImageSurface {
+    let surface = ImageSurface::create(Format::ARgb32, 400, 100).unwrap();
+    let cr = Context::new(&surface).unwrap();
+
+    let layout = pangocairo::functions::create_layout(&cr);//.unwrap();
+    layout.set_text(text);
+    let desc = FontDescription::from_string(&format!("{} {}", font, size));
+    layout.set_font_description(Some(&desc));
+
+    cr.set_source_rgb(1.0, 1.0, 1.0);
+    cr.paint().unwrap();
+    cr.set_source_rgb(0.0, 0.0, 0.0);
+    cr.move_to(10.0, 10.0);
+    pangocairo::functions::show_layout(&cr, &layout);
+
+    surface
+}
+
 #[defun]
 fn show_window(env: &Env) -> Result<Value<'_>> {
     INIT.call_once(|| {
@@ -31,19 +52,31 @@ fn show_window(env: &Env) -> Result<Value<'_>> {
     win.set_title("From Emacs (GTK3)");
     win.set_default_size(400, 300);
 
-    let btn = gtk::Button::with_label("Click me");
-    btn.connect_clicked({
-        //let window = win.clone();
-        let sender = sender.clone();
-        move |_| {
-            eprintln!("Button clicked from Emacs module");
-            //eprintln!("window! {:?}", window);
-            sender
-                .send_blocking(Event::Test)
-                .expect("cant send through channel");
-        }
+    // let btn = gtk::Button::with_label("Click me");
+    // btn.connect_clicked({
+    //     //let window = win.clone();
+    //     let sender = sender.clone();
+    //     move |_| {
+    //         eprintln!("Button clicked from Emacs module");
+    //         //eprintln!("window! {:?}", window);
+    //         sender
+    //             .send_blocking(Event::Test)
+    //             .expect("cant send through channel");
+    //     }
+    // });
+    //win.add(&btn);
+    let surface = render_text_offscreen("Hello from thread", "Sans", 24.0);
+    let area = gtk::DrawingArea::new();
+    area.connect_draw(move |_, cr| {
+        //if let Ok(surface) = rx.try_recv() {
+        // Only here does a window (via cr from DrawingArea) get involved
+        cr.set_source_surface(&surface, 0.0, 0.0).unwrap();
+        cr.paint().unwrap();
+        //}
+        gtk::glib::signal::Propagation::Stop
+        //Inhibit(false)
     });
-    win.add(&btn);
+    win.add(&area);
     win.show_all();
 
     glib::spawn_future_local(async move {
